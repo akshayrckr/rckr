8b5a266e2751663546323ceeca39fa9e284a9b33
akshayrckr <akshayrevankar@rckr.com>
2022-10-18 15:41:18 +0530

    created update and delete stored procedure

---
+++ StaffManagementDb/dbo/Stored Procedures/Proc_Staff_Update.sql
@@ -0,0 +1,28 @@
+﻿CREATE PROCEDURE Proc_Staff_Update
+@Name VARCHAR (50)=NULL,@Rolee VARCHAR(30),@StaffId int=NULL ,@Phone VARCHAR (30)=NULL, @Email VARCHAR (30)=NULL, @Address VARCHAR (10)=NULL, @Roles NVARCHAR (30), @Privelage NVARCHAR (50)=NULL, @Position NVARCHAR (50)=NULL,@Experience int=NULL, @Designation NVARCHAR (50)=NULL
+AS
+BEGIN
+    Update Staff SET Name = @Name, Email= @Email where StaffId=@StaffId;
+
+			Declare @RId int;
+			select @RId = RoleId from Roles where Role=@Rolee;
+
+			MERGE Admin as A
+			Using Staff as S
+			On S.StaffId = A.AId And S.StaffId = @StaffId and S.RoleId=@RId
+			When Matched then 
+			Update SET A.Privelage=@Privelage;
+
+			MERGE Teacher as T
+			Using Staff as S
+			On S.StaffId = T.TId And S.StaffId = @StaffId and S.RoleId=@RId
+			When Matched then 
+			Update SET T.Experience = @Experience;
+
+			MERGE Support as Su
+			Using Staff as S
+			On S.StaffId = Su.SId And S.StaffId = @StaffId and S.RoleId=@RId
+			When Matched then 
+			Update SET Su.Position=@Position;
+    COMMIT TRANSACTION;
+END
--- StaffManagementDb/dbo/Tables/Teacher.sql
+++ StaffManagementDb/dbo/Tables/Teacher.sql
@@ -1,7 +1,9 @@
 ﻿CREATE TABLE [dbo].[Teacher] (
-    [TId]       INT          NOT NULL,
-    [Privelage] VARCHAR (50) NULL,
+    [TId]        INT NOT NULL,
+    [Experience] INT NULL,
     PRIMARY KEY CLUSTERED ([TId] ASC),
     CONSTRAINT [fk_teacher_Staff] FOREIGN KEY ([TId]) REFERENCES [dbo].[Staff] ([StaffId]) ON DELETE CASCADE ON UPDATE CASCADE
 );
+
+
 
