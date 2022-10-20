56f933669da2d0486362cb73f3de72d91f04945a
akshayrckr <akshayrevankar@rckr.com>
2022-10-20 18:30:21 +0530

    Called all Stored Procedures from Vs

--- Data/SqlDataLayer.cs
+++ Data/SqlDataLayer.cs
@@ -36,7 +36,7 @@ namespace Data.Impl
             if (staffToCreate is Admin)
             {
                 cmd.Parameters.Add("@Privelage", SqlDbType.VarChar).Value = ((Admin)staffToCreate).Privelage;
-            }  
+            }
             if (staffToCreate is Teaching)
             {
                 cmd.Parameters.Add("@Experience", SqlDbType.VarChar).Value = ((Teaching)staffToCreate).Experience;
@@ -63,37 +63,38 @@ namespace Data.Impl
                 SqlCommand command = new SqlCommand("Proc_Staff_ReadById", cnn);
                 command.CommandType = CommandType.StoredProcedure;
                 command.Parameters.Add("@EmpId", SqlDbType.VarChar).Value = staffIdToRead;
-                
+
                 using (SqlDataReader reader = command.ExecuteReader())
                 {
-                   
+
                     do
                     {
-                        
+
 
                         while (reader.Read())
                         {
-                            string name = reader.GetValue(reader.GetOrdinal("name")).ToString();
+                            string Name = reader.GetValue(reader.GetOrdinal("Name")).ToString();
                             string Email = reader.GetValue(reader.GetOrdinal("Email")).ToString();
                             string Phone = reader.GetValue(reader.GetOrdinal("Phone")).ToString();
                             string Address = reader.GetValue(reader.GetOrdinal("Address")).ToString();
-                            string type = reader.GetValue(reader.GetOrdinal("Type")).ToString();
-                            
-                            switch (type)
+                            string Role = reader.GetValue(reader.GetOrdinal("Role")).ToString();
+
+                            switch (Role)
                             {
                                 case "Support":
                                     string position = reader.GetValue(reader.GetOrdinal("Position")).ToString();
-                                    Staff sp = new Support();
+                                    Staff sp = new Support(staffIdToRead, Name, Address, Email, Phone, position);
                                     return sp;
-                                    
+
+
                                 case "Admin":
-                                    string privelage = reader.GetValue(reader.GetOrdinal("Privelage")).ToString();
-                                    Staff ad = new Admin();
+                                    string privelage = reader["Privelage"].ToString();
+                                    Staff ad = new Admin(staffIdToRead, Name, Address, Email, Phone, privelage);
                                     return ad;
 
                                 case "Teacher":
-                                    string Experience = reader.GetValue(reader.GetOrdinal("Experience")).ToString();
-                                    Staff tc = new Teaching();
+                                    int Experience = (int)reader.GetValue(reader.GetOrdinal("Experience"));
+                                    Staff tc = new Teaching(staffIdToRead, Name, Address, Email, Phone, Experience);
                                     return tc;
 
                                 default:
@@ -101,25 +102,137 @@ namespace Data.Impl
                             }
                         }
                     } while (reader.NextResult());
-                    
+
                     command.ExecuteNonQuery();
                     cnn.Close();
                     return null;
                 }
             }
-            
 
-            
+
+
         }
 
         public List<Staff> ReadAll()
         {
-            throw new NotImplementedException();
+            List<Staff> Staffs = new List<Staff>();
+            using (SqlConnection cnn = new SqlConnection(connstr))
+            {
+                cnn.Open();
+                SqlCommand command = new SqlCommand("Proc_Staff_ReadAll", cnn);
+                command.CommandType = CommandType.StoredProcedure;
+               
+
+                using (SqlDataReader reader = command.ExecuteReader())
+                {
+
+                    do
+                    {
+
+
+                        while (reader.Read())
+                        {
+                            int StaffId = (int)reader.GetValue(reader.GetOrdinal("Staffid"));
+                            string Name = reader.GetValue(reader.GetOrdinal("Name")).ToString();
+                            string Email = reader.GetValue(reader.GetOrdinal("Email")).ToString();
+                            string Phone = reader.GetValue(reader.GetOrdinal("Phone")).ToString();
+                            string Address = reader.GetValue(reader.GetOrdinal("Address")).ToString();
+                            string Role = reader.GetValue(reader.GetOrdinal("Role")).ToString();
+
+                            switch (Role)
+                            {
+                                case "Support":
+                                    string position = reader["Position"].ToString();
+                                    Staff sp = new Support(StaffId, Name, Address, Email, Phone, position);
+                                    Staffs.Add(sp);
+                                    break;
+
+                                case "Admin":
+                                    string privelage = reader["Privelage"].ToString();
+                                    Staff ad = new Admin(StaffId, Name, Address, Email, Phone, privelage);
+                                    Staffs.Add(ad);
+                                    break;
+                                case "Teacher":
+                                    int Experience = (int)reader.GetValue(reader.GetOrdinal("Experience"));
+                                    Staff tc = new Teaching(StaffId, Name, Address, Email, Phone, Experience);
+                                    Staffs.Add(tc);
+                                    break;
+
+                                default:
+                                    break;
+                            }
+                        }
+                    } while (reader.NextResult());
+                   
+                    
+                    
+                    //command.ExecuteNonQuery();
+                    cnn.Close();
+                    return Staffs;
+                    
+
+                }
+            }
         }
 
         public List<Staff> ReadByType(string staffTypeToRead)
         {
-            throw new NotImplementedException();
+            List<Staff> staffs = new List<Staff>();
+            using (SqlConnection cnn = new SqlConnection(connstr))
+            {
+                cnn.Open();
+                SqlCommand command = new SqlCommand("Proc_Staff_ReadByType", cnn);
+                command.CommandType = CommandType.StoredProcedure;
+                command.Parameters.Add("@role", SqlDbType.VarChar).Value = staffTypeToRead;
+
+                using (SqlDataReader reader = command.ExecuteReader())
+                {
+
+                    do
+                    {
+
+
+                        while (reader.Read())
+                        {
+                            string Name = reader.GetValue(reader.GetOrdinal("Name")).ToString();
+                            string Email = reader.GetValue(reader.GetOrdinal("Email")).ToString();
+                            string Phone = reader.GetValue(reader.GetOrdinal("Phone")).ToString();
+                            string Address = reader.GetValue(reader.GetOrdinal("Address")).ToString();
+                            string Role = reader.GetValue(reader.GetOrdinal("Role")).ToString();
+                            int StaffId = (int)reader.GetValue(reader.GetOrdinal("StaffId"));
+
+                            switch (Role)
+                            {
+                                case "Support":
+                                    string position = reader.GetValue(reader.GetOrdinal("Position")).ToString();
+                                    Staff sp = new Support(StaffId, Name, Address, Email, Phone, position);
+                                    staffs.Add(sp);
+                                    break;
+
+
+                                case "Admin":
+                                    string privelage = reader["Privelage"].ToString();
+                                    Staff ad = new Admin(StaffId, Name, Address, Email, Phone, privelage);
+                                    staffs.Add(ad);
+                                    break;
+
+                                case "Teacher":
+                                    int Experience = (int)reader.GetValue(reader.GetOrdinal("Experience"));
+                                    Staff tc = new Teaching(StaffId, Name, Address, Email, Phone, Experience);
+                                    staffs.Add(tc);
+                                    break;
+
+                                default:
+                                    break;
+                            }
+                        }
+                    } while (reader.NextResult());
+                    return staffs;
+                    //command.ExecuteNonQuery();
+                    cnn.Close();
+                    return null;
+                }
+            }
         }
 
         public void Update(Staff staffToUpdate)
@@ -148,7 +261,5 @@ namespace Data.Impl
             cmd.ExecuteNonQuery();
             cnn.Close();
         }
-
-        
     }
 }
